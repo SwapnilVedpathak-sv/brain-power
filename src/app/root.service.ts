6975ff7583b22a588c309fa68252e49cfcb301cb
Swapnil Vedpathak <swapnilvedpathak.sv@gmail.com>
2021-06-30 00:11:19 +0530

    changed URL

--- src/app/root.service.ts
+++ src/app/root.service.ts
@@ -8,7 +8,7 @@ export class RootService {
 
   constructor(public http: HttpClient) { }
 
-  readonly baseUrl = 'http://localhost:8000';
+  readonly baseUrl = '';
   readonly AllCertificate = '/ndsCertificateData';
   readonly register = '/registerUser';
   readonly login = '/loginUser';
