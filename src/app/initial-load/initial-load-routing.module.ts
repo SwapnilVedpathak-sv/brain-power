f33e56c07a8e6baea09721e7a573bf08d70eb6b5
Swapnil Vedpathak <swapnilvedpathak.sv@gmail.com>
2021-07-09 20:06:07 +0530

    Added new splash screen

--- src/app/initial-load/initial-load-routing.module.ts
+++ src/app/initial-load/initial-load-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChoiceComponent } from './choice/choice.component';
 import { SplashScreenComponent } from './splash-screen/splash-screen.component';
 
-const routes: Routes = [{ path: '', component: SplashScreenComponent }];
+const routes: Routes = [
+  { path: '', component: SplashScreenComponent },
+  { path: 'makeYourChoices', component: ChoiceComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/initial-load/splash-screen/splash-screen.component.ts
+++ src/app/initial-load/splash-screen/splash-screen.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-splash-screen',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SplashScreenComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
+
+
+  windowWidth: string = '';
+  splashTransition: string = '';
+  opacityChange: number = 1;
+  showSplash = true;
+
+  @Input() animationDuration: number = 0.5;
+  @Input() duration: number = 3;
+  // @Input() animationType = SplashAnimationType.SlideLeft;
+
+  SlideLeft = "slide-left"
+  SlideRight = "slide-right"
+  FadeOut = "fade-out"
+
+  animationType = this.SlideLeft
 
   ngOnInit(): void {
-  }
+    setTimeout(() => {
+      let transitionStyle = "";
+      switch (this.animationType) {
+        case this.SlideLeft:
+          this.windowWidth = "-" + window.innerWidth + "px";
+          transitionStyle = "left " + this.animationDuration + "s";
+          break;
+        case this.SlideRight:
+          this.windowWidth = window.innerWidth + "px";
+          transitionStyle = "left " + this.animationDuration + "s";
+          break;
+        case this.FadeOut:
+          transitionStyle = "opacity " + this.animationDuration + "s";
+          this.opacityChange = 0;
+      }
 
+      this.splashTransition = transitionStyle;
+
+      setTimeout(() => {
+        this.showSplash = !this.showSplash;
+      }, this.animationDuration * 1000);
+
+      this.router.navigate(['makeYourChoices'])
+    }, this.duration * 1000);
+  }
 }
